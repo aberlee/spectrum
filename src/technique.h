97754db52a1ebea519e55c2ea92e210ca0be9112
Rena <renashinomiya@gmail.com>
2018-03-19 21:39:49 -0500

    Add initial technique header files

---
+++ src/technique.h
@@ -0,0 +1,118 @@
+/**********************************************************//**
+ * @file technique.h
+ * @brief Lists all the techniques usable by monsters.
+ * @author Rena Shinomiya
+ * @date March 19th, 2018
+ **************************************************************/
+
+#ifndef _TECHNIQUE_H_
+#define _TECHNIQUE_H_
+
+#include "type.h"           // TYPE_ID
+#include "effect.h"         // TARGET_ID, EFFECT_ID
+
+/**********************************************************//**
+ * @enum TECHNIQUE_ID
+ * @brief Defines a constant for each different technique
+ * that can be learned by a monster (0 is invalid).
+ **************************************************************/
+typedef enum {
+    // Basic techniques
+    BITE=1,
+    COLLIDE,
+    SLICE,
+    RAMPAGE,
+    ENGULF,
+    MANGLE,
+    BOMBARD,
+    TRI_BEAM,
+    FLATTEN,
+    CRUSH,
+    RECOVER,
+    PURGE,
+    DISPEL,
+    SHARPEN,
+    SNIPER,
+    PREVENT,
+    DISARM,
+    LURK,
+    PROVOKE,
+    EXPLOSION,
+    
+    // Wind techniques
+    GUST,
+    CYCLONE,
+    HURRICANE,
+    
+    // Fire techniques
+    HOT_GUST,
+    FIREWALL,
+    INFERNO,
+    
+    // Water techniques
+    SWIM_ATTACK,
+    SURGE,
+    RIPTIDE,
+    MAELSTROM,
+    TSUNAMI,
+    
+    // Dark techniques
+    POISON_CLAW,
+    INKY_BANE,
+    GUNK_ATTACK,
+    POISON_GRIP,
+    SMOG,
+    
+    // Light techniques
+    ZAP,
+    LIGHTNING,
+    THUNDER,
+    CHARGE,
+    
+    // Ice techniques
+    COLD_GUST,
+    AVALANCHE,
+    
+    // Mind techniques
+    MIND_RAY,
+    BRAINWAVE,
+    HYPNOSIS,
+    MIND_SCREW,
+    OBSCURE,
+    
+    // Earth techniques
+    FRACTURE,
+    EARTHQUAKE,
+    STRYATION,
+    
+    // Metal techniques
+    CORE_BEAM,
+    IRON_ATTACK,
+    IRON_HEAVE,
+    SOLIDIFY,
+    
+    // Extra techniques
+    CAPTURE,
+    AILMENT,
+    FIRE_BREATH,
+    RECYCLE,
+} TECHNIQUE_ID;
+
+/**********************************************************//**
+ * @struct TECHNIQUE
+ * @brief Stores all constant data about one technique
+ * specified with a TECHNIQUE_ID.
+ **************************************************************/
+typedef struct {
+    char Name[12];          ///< The name of the technique.
+    TYPE_ID Type;           ///< The type of the technique.
+    int Power;              ///< Technique power (0 is none).
+    int Cost;               ///< Cost of the technique (0 is free).
+    TARGET_TYPE Target;     ///< Who the technique targets.
+    EFFECT_ID Effect;       ///< Effect on the targets.
+    int Argument;           ///< Argument to the effect.
+    char Description[32];   ///< Describes the technique on the menu.
+} TECHNIQUE;
+
+/**************************************************************/
+#endif
---
+++ src/type.h
@@ -0,0 +1,30 @@
+/**********************************************************//**
+ * @file type.h
+ * @brief Defines universal types that are used across the
+ * species and techniques.
+ * @author Rena Shinomiya
+ * @date March 19th, 2018
+ **************************************************************/
+
+#ifndef _TYPE_H_
+#define _TYPE_H_
+
+/**********************************************************//**
+ * @enum TYPE_ID
+ * @brief Defines a constant for each type used, starting
+ * at 1 (0 is invalid).
+ **************************************************************/
+typedef enum {
+    BASIC=1,    ///< Basic type - unclassifiable or typeless.
+    FIRE,       ///< Fire type - heat-related.
+    WATER,      ///< Water type - marine life and liquids.
+    ICE,        ///< Ice type - anything cold.
+    WIND,       ///< Wind type - weather and birds.
+    EARTH,      ///< Earth type - geology or dirt related.
+    METAL,      ///< Metal-type - mineral or machine related.
+    LIGHT,      ///< Light-type - based on electricity and energy.
+    DARK,       ///< Dark-type - based on night, poison, and fear.
+} TYPE_ID;
+
+/**************************************************************/
+#endif
