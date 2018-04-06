7cf73b97995a7d9220f319bad0bcd25c055c9314
Rena <8904131+xyRena@users.noreply.github.com>
2018-04-05 19:40:50 -0500

    Move display constants into their own header

---
+++ src/display.h
@@ -0,0 +1,16 @@
+/**********************************************************//**
+ * @file display.h
+ * @brief Header for basic screen information.
+ * @author Rena Shinomiya
+ * @date April 5th, 2018
+ **************************************************************/
+
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
+#define DISPLAY_WIDTH 480
+#define DISPLAY_HEIGHT 360
+#define FRAME_RATE 60.0
+
+/**************************************************************/
+#endif // _DISPLAY_H_
