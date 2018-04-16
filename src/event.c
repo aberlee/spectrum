9fbb75579d63c7b89d05e46346484d1dd964461b
Rena <8904131+xyRena@users.noreply.github.com>
2018-04-15 19:02:13 -0500

    Document event file

--- src/event.c
+++ src/event.c
@@ -8,6 +8,13 @@
 #include "event.h"              // EVENT
 #include "event.i"              // EVENT_DATA
 
+/**********************************************************//**
+ * @brief Gets the events for the given map.
+ * @param id: The map identity to get events on.
+ * @return Pointer to the constant event data.
+ **************************************************************/
 const EVENT* Events(MAP_ID id) {
     return EVENT_DATA[id];
 }
+
+/**************************************************************/
